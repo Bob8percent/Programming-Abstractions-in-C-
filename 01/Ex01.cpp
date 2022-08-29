aceac8ed89a11cd29eff52cf27daf1f45744f243
Bob8percent <53217759+Bob8percent@users.noreply.github.com>
2022-08-29 15:43:00 +0900

    Update Ex01.cpp

--- 01/Ex01.cpp
+++ 01/Ex01.cpp
@@ -1,1 +1,27 @@
 
+/*
+    入力した摂氏温度に対して、華氏温度を出力するプログラム
+*/
+
+#include <iostream>
+
+double calcFahrenheit(double celsius);
+
+int main()
+{
+    double celsius = 0.0;
+
+    while (true)
+    {
+        std::cout << "摂氏温度を入力：";
+        std::cin >> celsius;
+
+        std::cout << calcFahrenheit(celsius) << std::endl;
+    }
+}
+
+double calcFahrenheit(double celsius)
+{
+    return (1.8 * celsius + 32.0);
+}
+
