3574a6a0ef45844817c3a2e97d2e6a639204d159
Bob8percent <53217759+Bob8percent@users.noreply.github.com>
2022-09-03 13:50:14 +0900

    Create calender.cpp

---
+++ 02/Ex11/calender.cpp
@@ -0,0 +1,88 @@
+#include "calender.h"
+
+int daysInMonth(int year, Month month)
+{
+	int days = -1;
+	switch (month)
+	{
+	case FEBRUARY:
+		days = (isLeapYear(year) ? 29 : 28);
+		break;
+
+	case JANUARY:
+	case MARCH:
+	case MAY:
+	case JULY:
+	case AUGUST:
+	case OCTOBER:
+	case DECEMBER:
+		days = 31;
+		break;
+		
+	case APRIL:
+	case JUNE:
+	case SEPTEMBER:
+	case NOVEMBER:
+		days = 30;
+		break;
+
+	default:
+		break;
+	}
+
+	return days;
+}
+
+bool isLeapYear(int year)
+{
+	return(year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+}
+
+std::string monthToString(Month month)
+{
+	std::string str;
+
+	switch (month)
+	{
+	case FEBRUARY:
+		str = "FEBRUARY";;
+		break;
+	case JANUARY:
+		str = "JANUARY";;
+		break;
+	case MARCH:
+		str = "MARCH";;
+		break;
+	case MAY:
+		str = "MAY";;
+		break;
+	case JULY:
+		str = "JULY";;
+		break;
+	case AUGUST:
+		str = "AUGUST";;
+		break;
+	case OCTOBER:
+		str = "OCTOBER";;
+		break;
+	case DECEMBER:
+		str = "DECEMBER";;
+		break;
+	case APRIL:
+		str = "APRIL";;
+		break;
+	case JUNE:
+		str = "JUNE";;
+		break;
+	case SEPTEMBER:
+		str = "SEPTEMBER";;
+		break;
+	case NOVEMBER:
+		str = "NOBEMBER";;
+		break;
+	default:
+		break;
+	}
+
+	return str;
+}
