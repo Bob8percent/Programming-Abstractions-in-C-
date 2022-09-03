f83ac66dbc54db38533b432792fd47c164212f6a
Bob8percent <53217759+Bob8percent@users.noreply.github.com>
2022-09-04 04:11:13 +0900

    Create PigLatin.h

---
+++ 03/PigLatin/PigLatin.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+
+/*
+* 文字列をPigLatinへ翻訳する関数
+*/
+std::string lineToPigLatin(const std::string& str);
+
+/*
+* 英単語をPigLatinへ翻訳する関数
+*/
+std::string wordToPigLatin(const std::string& word);
