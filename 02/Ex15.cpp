b3996a52d07e9b9ceb9aea19b36e430b782eb740
Bob8percent <53217759+Bob8percent@users.noreply.github.com>
2022-09-03 14:59:02 +0900

    Create Ex15.cpp

---
+++ 02/Ex15.cpp
@@ -0,0 +1,53 @@
+
+/*
+* モンテカルロ法によりpiを導出するプログラム
+*/
+
+#include <iostream>
+#include <iomanip>
+
+bool randomChance(double p);
+void initRand();
+
+int main()
+{
+	const int numConsecutive = 3;	//	numHeadsの最大値
+
+	int numHeads = 0;	//	連続で表が出た回数
+	int count = 0;		//	試行回数
+	
+	while (numHeads < numConsecutive)
+	{
+		if (randomChance(0.5))
+		{
+			std::cout << "表がでた\n";
+			++numHeads;
+		}
+		else
+		{
+			std::cout << "裏がでた\n";
+			numHeads = 0;
+		}
+
+		++count;
+	}
+
+	std::cout << numConsecutive << "回連続で表が出るまで" << count << "回かかった\n";
+}
+
+bool randomChance(double p)
+{
+	initRand();
+
+	return (rand() / (RAND_MAX * 1.0)) <= p;
+}
+
+void initRand()
+{
+	static bool isFirst = true;
+	if (isFirst)
+	{
+		srand((int)time(NULL));
+		isFirst = false;
+	}
+}
