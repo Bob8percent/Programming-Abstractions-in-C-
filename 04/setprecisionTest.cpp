48d6feea0278b33f09a0e5734e976e8c5c3a99e8
Bob8percent <53217759+Bob8percent@users.noreply.github.com>
2022-09-05 23:22:41 +0900

    Create setprecisionTest.cpp

---
+++ 04/setprecisionTest.cpp
@@ -0,0 +1,42 @@
+
+/*
+* setprecisionの挙動を確認する
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "String.h"
+
+using std::setprecision;
+const double PI = 3.14159265358979323846;
+const double SPEED_OF_LIGHT = 2.99792458e+8;
+const double FINE_STRUCTURE = 7.2573525e-3;
+
+void printPrecisionTable();
+
+int main()
+{
+  //  デフォルト
+	std::cout << "Default format:" << std::endl << std::endl;
+	printPrecisionTable();
+
+  //  fixed状態のsetprecision
+	std::cout << std::endl << "Fixed format:" << std::fixed << std::endl << std::endl;
+	printPrecisionTable();
+
+  //  scientific状態の
+	std::cout << std::endl << "Scientific format:" << std::scientific << std::endl << std::endl;
+	printPrecisionTable();
+}
+
+void printPrecisionTable()
+{
+	for (int precision = 0; precision <= 6; precision += 2)
+	{
+		std::cout << "setprecision(" << precision << ")の場合、" << std::endl;
+		std::cout << "PI : " << setprecision(precision) << PI << std::endl;
+		std::cout << "SPEED_OF_LIGHT : " << setprecision(precision) << SPEED_OF_LIGHT << std::endl;
+		std::cout << "FINE_STRUCTURE : " << setprecision(precision) << FINE_STRUCTURE << std::endl;
+	}
+}
