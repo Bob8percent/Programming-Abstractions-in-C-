dca882b54e834f1c1c33dd54a1678131417c3065
Bob8percent <53217759+Bob8percent@users.noreply.github.com>
2022-09-10 21:27:52 +0900

    Create Ex15.cpp

---
+++ 05/Ex15.cpp
@@ -0,0 +1,58 @@
+//	Stackの最上位n個の要素をk回だけ回転させる
+void roll(Stack<char>& stack, int n, int k);
+void printStack(Stack<char> stack);
+
+int main()
+{
+	Stack<char> stack;
+	for (char ch = 'A'; ch <= 'Z'; ++ch)stack.push(ch);
+	while (1)
+	{
+		std::cout << "現在：";
+		printStack(stack);
+
+		std::cout << " n : ";
+		int n = getInteger(std::cin);
+		std::cout << "k : ";
+		int k = getInteger(std::cin);
+
+		std::cout << "roll(stack, " << n << ", " << k << ")を実行します" << std::endl;
+		roll(stack, n, k);
+	}
+}
+
+void printStack(Stack<char> stack)
+{
+	std::cout << "上位 " ;
+	while (!stack.isEmpty())std::cout << stack.pop();
+	std::cout << " 底" << std::endl;
+}
+
+void roll(Stack<char>& stack, int n, int k)
+{
+	if (stack.size() < n)
+	{
+		std::cout << "ERROR: void roll(Stack<char>& stack, int n, int k) : "
+			<< "nがstackのサイズを超えています" << std::endl;
+		return;
+	}
+	if (!(n > 0 && k > 0))
+	{
+		std::cout << "ERROR: void roll(Stack<char>& stack, int n, int k) : "
+			<< "nまたはkが負となっています" << std::endl;
+		return;
+	}
+
+	std::string rotatedData(n, '0');
+	int shift = n - k % n;
+	for (int i = shift; i < n + shift; ++i)
+	{
+		int k = i % n;
+		rotatedData[k] = stack.pop();
+	}
+
+	for (int i = n - 1; i >= 0; --i)
+	{
+		stack.push(rotatedData[i]);
+	}
+}
