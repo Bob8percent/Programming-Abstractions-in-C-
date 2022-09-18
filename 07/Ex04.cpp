c918cf31383cacc90e49a619bf4a36efe4d51756
Bob8percent <53217759+Bob8percent@users.noreply.github.com>
2022-09-18 20:15:05 +0900

    Create Ex04.cpp

---
+++ 07/Ex04.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cctype>
+#include <sstream>
+#include <fstream>
+#include "TokenScanner.h"
+#include "Library/queue.h"
+#include "Library/map.h"
+
+int gcd(int x, int y);
+
+int main()
+{
+	std::cout << gcd(50, 40) << std::endl;
+}
+
+int gcd(int x, int y)
+{
+	if (x < 0 || y < 0)
+	{
+		std::cerr << "ERROR : int gcd(int x, int y) : xまたはyが負になっています" << std::endl;
+		std::exit(EXIT_FAILURE);
+	}
+	int d = x % y;
+	if (d == 0)return y;
+	return gcd(y, d);
+}
