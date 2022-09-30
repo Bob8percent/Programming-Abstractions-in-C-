c73a702f59de02b3a1159b513f7a040ca0475416
Bob8percent <53217759+Bob8percent@users.noreply.github.com>
2022-10-01 06:08:58 +0900

    Create main.cpp

---
+++ 09/Ex15/main.cpp
@@ -0,0 +1,21 @@
+#define _USE_MATH_DEFINES
+
+#include <iostream>
+#include <cctype>
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+#include <cmath>
+#include <SFML/Graphics.hpp>
+#include "TokenScanner.h"
+#include "Library/queue.h"
+#include "Library/map.h"
+#include "Library/set.h"
+#include "TicTacToe.h"
+
+int main()
+{
+	TicTacToe game(HUMAN);
+	game.play();
+}
+
