7fb77b965cd6ebb2ac806e96ce96031622f99847
Bob8percent <53217759+Bob8percent@users.noreply.github.com>
2022-10-11 04:14:02 +0900

    Create BeaconsOfGondor.cpp

---
+++ 12/BeaconsOfGondor.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cctype>
+#include <sstream>
+#include <fstream>
+#include "TokenScanner.h"
+#include "Library/stack.h"
+
+struct Tower {
+	std::string name;
+	Tower* link;
+};
+
+Tower* createBeaconOfGondor();
+Tower* createTower(const std::string& name, Tower* link);
+
+int main() {
+	Tower* tp = createBeaconOfGondor();
+
+	while (tp) {
+		std::cout << tp->name << std::endl;
+		tp = tp->link;
+	}
+}
+
+Tower* createBeaconOfGondor() {
+	Tower* tp = createTower("Rohan", nullptr);
+	tp = createTower("Halifirien", tp);
+	tp = createTower("Calenhad", tp);
+	tp = createTower("Min-Rimmon", tp);
+	tp = createTower("Erelas", tp);
+	tp = createTower("Nardol", tp);
+	tp = createTower("Eilenach", tp);
+	tp = createTower("Amon Din", tp);
+	tp = createTower("Minas Tirith", tp);
+
+	return tp;
+}
+
+Tower* createTower(const std::string& name, Tower* link) {
+	Tower* tower = new Tower;
+	tower->name = name;
+	tower->link = link;
+
+	return tower;
+}
