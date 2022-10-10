82ef5dfdf9aa6854a0c2eac0b8a55b9457270ca1
Bob8percent <53217759+Bob8percent@users.noreply.github.com>
2022-10-11 05:41:11 +0900

    Create CharStack.h

---
+++ 12/CharStack/CharStack.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "Library/vector.h"
+
+class CharStack
+{
+public:
+	CharStack();
+	~CharStack();
+
+	int size();
+	bool isEmpty();
+	void clear();
+	
+	void push(char ch);
+	char pop();
+
+	char peek();	//	最上の要素を返すが、それはスタックから除外しない
+
+private:
+	static const int INITIAL_CAPACITY = 10;
+	char* elements;
+	int count;	//	要素数
+	int capacity;
+
+	void expandCapacity();
+};
+
