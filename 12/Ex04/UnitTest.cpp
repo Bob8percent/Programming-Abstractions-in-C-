cb8df09a5251a59ddd64e4716673b9b26cd7659b
Bob8percent <53217759+Bob8percent@users.noreply.github.com>
2022-10-12 04:31:41 +0900

    Create UnitTest.cpp

---
+++ 12/Ex04/UnitTest.cpp
@@ -0,0 +1,76 @@
+#include "UnitTest.h"
+#include <cassert>
+#include <iostream>
+
+#if 0
+#include "CharStack.h"
+
+void charStackTest() {
+	CharStack cstk;
+	assert(cstk.size() == 0);
+	assert(cstk.isEmpty());
+
+	cstk.push('A');
+	assert(!cstk.isEmpty());
+	assert(cstk.size() == 1);
+	assert(cstk.peek() == 'A');
+	cstk.push('B');
+	assert(cstk.peek() == 'B');
+	assert(cstk.size() == 2);
+	assert(cstk.pop() == 'B');
+	assert(cstk.size() == 1);
+	assert(cstk.peek() == 'A');
+	cstk.push('C');
+	assert(cstk.size() == 2);
+	assert(cstk.pop() == 'C');
+	assert(cstk.peek() == 'A');
+	assert(cstk.pop() == 'A');
+	assert(cstk.size() == 0);
+
+	for (char ch = 'A'; ch <= 'Z'; ++ch) {
+		cstk.push(ch);
+	}
+	assert(cstk.size() == 26);
+
+	for (char ch = 'Z'; ch >= 'A'; --ch) {
+		assert(cstk.pop() == ch);
+	}
+	assert(cstk.isEmpty());
+
+	for (char ch = 'A'; ch <= 'Z'; ++ch) {
+		cstk.push(ch);
+	}
+	assert(cstk.size() == 26);
+
+	cstk.clear();
+	assert(cstk.size() == 0);
+	cstk.clear();
+	assert(cstk.size() == 0);
+
+	std::cout << "CharStack unit test succeeded." << std::endl;
+}
+#endif
+
+#if 1
+#include "IntArray.h"
+
+void intArrayTest() {
+	//IntArray zero(0);
+
+	IntArray a(10);
+
+	assert(a.size() == 10);
+	assert(a.get(5) == 0);
+
+	//a.get(10);
+
+	a.put(6, 66);
+	assert(a.get(6) == 66);
+
+	//a.put(10, 100);
+
+	std::cout << "IntArray unit test succeeded." << std::endl;
+}
+#endif
+
+
