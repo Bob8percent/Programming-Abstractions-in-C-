2ec44fb8bc1bf5bf8de172087260d1290e1e14c7
Bob8percent <53217759+Bob8percent@users.noreply.github.com>
2022-10-12 04:32:16 +0900

    Create IntArray.cpp

---
+++ 12/Ex04/IntArray.cpp
@@ -0,0 +1,41 @@
+#include "IntArray.h"
+#include <iostream>
+
+IntArray::IntArray(int n) {
+	if (n <= 0) {
+		std::cerr << "ERROR : IntArray::IntArray(int n) : "
+			<< "nが0以下となっています" << std::endl;
+		std::exit(EXIT_FAILURE);
+	}
+	elements = new int[n];
+	for (int i = 0; i < n; ++i)elements[i] = 0;
+	this->n = n;
+}
+
+IntArray::~IntArray() {
+	delete[] elements;
+}
+
+int IntArray::size() const {
+	return n;
+}
+
+int IntArray::get(int k) const {
+	if (k < 0 || n <= k) {
+		std::cerr << "ERROR : int IntArray::get(int k) const : "
+			<< "指定したインデックスkの値が範囲外です" << std::endl;
+		std::exit(EXIT_FAILURE);
+	}
+
+	return elements[k];
+}
+
+void IntArray::put(int k, int value) {
+	if (k < 0 || n <= k) {
+		std::cerr << "ERROR : int IntArray::put(int k, int value) : "
+			<< "指定したインデックスkの値が範囲外です" << std::endl;
+		std::exit(EXIT_FAILURE);
+	}
+
+	elements[k] = value;
+}
