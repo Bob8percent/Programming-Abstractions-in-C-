706ac57d2dac9d0766f2b36da06ebc803f84ca3b
Bob8percent <53217759+Bob8percent@users.noreply.github.com>
2022-10-12 04:51:04 +0900

    Create IntArray.h

---
+++ 12/Ex05/IntArray.h
@@ -0,0 +1,23 @@
+#pragma once
+class IntArray
+{
+public:
+	IntArray(int n);
+	~IntArray();
+
+	int size() const;
+
+	int get(int k) const;
+	void put(int k, int value);
+
+	int& operator[](int k);
+
+private:
+	int n;
+	int* elements;
+
+	//	コピー、代入を禁止
+	IntArray(const IntArray& src) {}
+	void operator=(const IntArray& src) {}
+};
+
