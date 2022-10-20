89d4e3b6ee3f6ab5a4a8f94198bcbb5268accc8d
Bob8percent <53217759+Bob8percent@users.noreply.github.com>
2022-10-20 09:50:32 +0900

    Update EditorBuffer.cpp

--- 13/EditorBuffer/stack-based editor/EditorBuffer.cpp
+++ 13/EditorBuffer/stack-based editor/EditorBuffer.cpp
@@ -6,25 +6,32 @@ EditorBuffer::EditorBuffer() {
 EditorBuffer::~EditorBuffer() {
 }
 
+//	O(1)
 void EditorBuffer::moveCursorForward() {
 	if (!after.isEmpty())before.push(after.pop());
 }
+
+//	O(1)
 void EditorBuffer::moveCursorBackward() {
 	if (!before.isEmpty())after.push(before.pop());
 }
 
+//	O(N)
+//	課題：文字列の最初(後)から最後(初)まで移動したとき、文字数の2倍のメモリが使用される可能性がある
 void EditorBuffer::moveCursorToStart() {
 	while (!before.isEmpty())after.push(before.pop());
 }
-
+//	O(N)
 void EditorBuffer::moveCursorToEnd() {
 	while (!after.isEmpty())before.push(after.pop());
 }
 
+//	O(1)
 void EditorBuffer::insertCharacter(char ch) {
 	before.push(ch);
 }
 
+//	O(1)
 void EditorBuffer::deleteCharacter() {
 	if (before.isEmpty() && after.isEmpty()) return;
 	if (!after.isEmpty()) {
