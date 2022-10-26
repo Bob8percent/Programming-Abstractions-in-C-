1012a1d392f846aa286c68be7d00acb82b4c9bc8
Bob8percent <53217759+Bob8percent@users.noreply.github.com>
2022-10-26 16:05:11 +0900

    Create UnitTest.h

---
+++ 14/Ex08/UnitTest.h
@@ -0,0 +1,17 @@
+#pragma once
+
+void charStackTest();
+
+void intArrayTest();
+
+void myStringTest();
+
+void rationalTest();
+
+void editorBufferTest();
+
+void stackTest();
+
+void queueTest();
+
+void pQueueTest();
