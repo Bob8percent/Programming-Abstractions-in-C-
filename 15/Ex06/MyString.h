ab54198599ce7fd4216089514b36f5ef83225fae
Bob8percent <53217759+Bob8percent@users.noreply.github.com>
2022-10-29 04:05:01 +0900

    Create MyString.h

---
+++ 15/Ex06/MyString.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <string>
+#include "Library/vector.h"
+
+class StringMap
+{
+public:
+	StringMap();
+	~StringMap();
+	std::string get(const std::string& key) const;
+	void put(const std::string& key, const std::string& value);
+
+	void hide(const std::string& key);	//	一時的に別のvalueを挿入
+	void restore(const std::string& key);	//	hideを解除
+
+	void debug();	//	各バケットの個数を求めるデバッグ関数
+
+private:
+	//	データ構造：hashTable
+	struct Cell {
+		std::string key;
+		std::string value;
+		Cell* link;
+	};
+	static const int INITIAL_BUCKET_COUNT = 13;
+	int nBuckets;
+	Cell** buckets;
+	int count;
+
+	int hashCode(const std::string& key) const;
+	Cell* findCell(int bucket, const std::string& key) const;
+
+	void reHash();
+};
