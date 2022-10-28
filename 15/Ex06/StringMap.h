d32ad07420d8faa130296fecf6e887efac8f14f1
Bob8percent <53217759+Bob8percent@users.noreply.github.com>
2022-10-29 05:19:13 +0900

    Update StringMap.h

--- 15/Ex06/StringMap.h
+++ 15/Ex06/StringMap.h
@@ -10,8 +10,21 @@ public:
 	std::string get(const std::string& key) const;
 	void put(const std::string& key, const std::string& value);
 
-	void hide(const std::string& key);	//	一時的に別のvalueを挿入
-	void restore(const std::string& key);	//	hideを解除
+	//	------------コメントについて----------
+	//	インターフェースに書くときはクライアントへ、実装部分に書くときは開発者へ向けたコメントを書くべき！
+	//	----------------------------------
+	// 
+	// 
+	//	add関数：
+	//	key,valueを持つcellをバケットの先頭に追加する関数。
+	//	これを使えば、keyを持つcellについて、valueに上書きできる。この実装には、findKeyが最初に見つけたkeyのcellを扱う仕組みであることを利用している
+	//	呼び出した分だけ先頭にcellが増えていくことに注意。
+	void add(const std::string& key, const std::string& value);	//	一時的に別のvalueを挿入
+
+	//	remove関数：
+	//	バケットの先頭にあり、keyをキーに持つcellを削除する関数。
+	//	addで作成したcellを削除することを目的として作った。
+	void remove(const std::string& key);	//	addしたcellを除去
 
 	void debug();	//	各バケットの個数を求めるデバッグ関数
 
