d3494d2ecc6c08d9b48b5d6d4427e01651a3bfa9
Bob8percent <53217759+Bob8percent@users.noreply.github.com>
2022-10-28 18:19:20 +0900

    Create StringMap.h

---
+++ 15/Ex01/StringMap.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+#include "Library/vector.h"
+
+class StringMap
+{
+public:
+	StringMap();
+	~StringMap();
+	std::string get(const std::string& key) const;
+	void put(const std::string& key, const std::string& value);
+
+private:
+	//	データ構造：vector
+	//	pairsをソートした状態で保ち、findKey()で2分探索法を用いればO(logN)に短縮できるが、
+	//	putを実行するたびにシフトが必要なのでO(N)になってしまう。
+	struct Pair {
+		std::string key;
+		std::string value;
+	};
+
+	Vector<Pair> pairs;
+
+	int findKey(const std::string& key) const;
+	int findKey(const std::string& key, int start, int end) const;
+};
