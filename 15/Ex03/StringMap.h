2c5b8b600dd46c7197f59dc53fa09076a4380bfa
Bob8percent <53217759+Bob8percent@users.noreply.github.com>
2022-10-29 01:05:23 +0900

    Create StringMap.h

---
+++ 15/Ex03/StringMap.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <string>
+
+class StringMap
+{
+public:
+	StringMap();
+	StringMap(const StringMap& src);
+	~StringMap();
+
+	std::string get(const std::string& key) const;
+	void put(const std::string& key, const std::string& value);
+
+	int size() const;
+	bool isEmpty() const;
+	bool containsKey(const std::string& key) const;
+	void clear();
+
+	StringMap& operator=(const StringMap& src);
+
+private:
+	//	データ構造：動的配列
+	struct Pair {
+		std::string key;
+		std::string value;
+	};
+	static const int INTIAL_CAPACITY = 10;
+	int capacity;
+	int count;
+	Pair* pairs;
+
+	int findKey(const std::string& key) const;
+	int findKey(const std::string& key, int start, int end) const;
+
+	void expandCapacity();
+	void deepCopy(const StringMap& src);
+};
