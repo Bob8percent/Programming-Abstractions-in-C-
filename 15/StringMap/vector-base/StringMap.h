4f6f5606fb37d6a6334087063e59967ff19c96af
Bob8percent <53217759+Bob8percent@users.noreply.github.com>
2022-10-28 10:21:37 +0900

    Update StringMap.h

--- 15/StringMap/vector-base/StringMap.h
+++ 15/StringMap/vector-base/StringMap.h
@@ -11,6 +11,9 @@ public:
 	void put(const std::string& key, const std::string& value);
 
 private:
+	//	データ構造：vector
+	//	pairsをソートした状態で保ち、findKey()で2分探索法を用いればO(logN)に短縮できるが、
+	//	putを実行するたびにシフトが必要なのでO(N)になってしまう。
 	struct Pair {
 		std::string key;
 		std::string value;
