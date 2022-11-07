b0e7e520873ff1328d00f92a9cb227296fb4b17c
Bob8percent <53217759+Bob8percent@users.noreply.github.com>
2022-11-08 00:22:12 +0900

    Create BinarySearchTree.h

---
+++ 16/Ex09/BinarySearchTree.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <string>
+
+//	二分探索木：常に整列された二分木
+class BinarySearchTree
+{
+public:
+	struct BSTNode {
+		int bf;	//	balance factor(右のサブツリーの高さ - 左のサブツリーの高さ)
+		std::string key;
+		BSTNode* left;
+		BSTNode* right;
+	};
+
+	BinarySearchTree();
+	~BinarySearchTree();
+
+	void displayKey() const;
+
+	//BSTNode* findNode(BSTNode* node, const std::string& key) const;
+	void insertNode(const std::string& key);
+
+	void debug();
+
+private:
+	BSTNode* findNode(BSTNode* node, const std::string& key) const;
+	void insertNode(BSTNode*& node, const std::string& key);
+	int insertAVL(BSTNode*& node, const std::string& key);
+	void removeNode(const std::string& key);
+	BSTNode* remove(BSTNode*& node, int& direction, const std::string& key);
+	int reBalance(BSTNode*& node, const BSTNode* actionPos, const int direction);
+
+	void fixRightImBalance(BSTNode*& node);
+	void fixLeftImBalance(BSTNode*& node);
+	void rotateRight(BSTNode*& node);
+	void rotateLeft(BSTNode*& node);
+
+	void displayKey(BSTNode* node, int height) const;
+
+	void clear(BSTNode* node);
+
+	int height(BSTNode* node) const;
+	bool isLeaf(BSTNode* node) const;
+
+	bool isBalanced(BSTNode* node) const;
+
+	bool hasBinarySearchProperty(BSTNode* tree);
+
+	BSTNode* root;
+};
+
