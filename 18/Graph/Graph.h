0906c106c3778c9f2ca137b2ad080490252df9af
Bob8percent <53217759+Bob8percent@users.noreply.github.com>
2022-11-14 03:57:57 +0900

    Create Graph.h

---
+++ 18/Graph/Graph.h
@@ -0,0 +1,275 @@
+#pragma once
+#include <set>
+#include <map>
+#include <string>
+#include <iostream>
+#include "Library/vector.h"
+
+template<typename NodeType, typename ArcType>
+class Graph {
+public:
+	Graph();
+	Graph(const Graph<NodeType, ArcType>& src);
+	~Graph();
+
+	int size() const;
+	bool isEmpty() const;
+	void clear();
+
+	void addNode(const std::string& name);
+	void addNode(NodeType* node);
+
+	void removeNode(const std::string& name);
+	void removeNode(NodeType* node);
+
+	NodeType* getNode(const std::string& name) const;
+
+	void addArc(const std::string& s1, const std::string& s2);
+	void addArc(NodeType* n1, NodeType* n2);
+	void addArc(ArcType* arc);
+
+	void removeArc(const std::string& s1, const std::string& s2);
+	void removeArc(NodeType* n1, NodeType* n2);
+	void removeArc(ArcType* arc);
+
+	bool isConnected(const std::string& s1, const std::string& s2) const;
+	bool isConnected(NodeType* n1, NodeType* n2) const;
+
+	std::set<NodeType*>& getNodeSet() const;
+	std::set<ArcType*>& getArcSet() const;
+	std::set<ArcType*>& getArcSet(const std::string& name) const;
+	std::set<ArcType*>& getArcSet(NodeType* node) const;
+
+	std::set<NodeType*> getNeighbors(const std::string& name) const;
+	std::set<NodeType*> getNeighbors(NodeType* node) const;
+
+	Graph<NodeType, ArcType>& operator=(const Graph<NodeType, ArcType>& src);
+
+private:
+	std::set<NodeType*> nodes;
+	std::set<ArcType*> arcs;
+	std::map<std::string, NodeType*> nodeMap;
+
+	void deepCopy(const Graph<NodeType, ArcType>& src);
+
+};
+
+template<typename NodeType, typename ArcType>
+Graph<NodeType, ArcType>::Graph() {
+}
+
+template<typename NodeType, typename ArcType>
+Graph<NodeType, ArcType>::Graph(const Graph<NodeType, ArcType>& src) {
+	deepCopy(src);
+}
+
+template<typename NodeType, typename ArcType>
+Graph<NodeType, ArcType>::~Graph() {
+	clear();
+}
+
+template<typename NodeType, typename ArcType>
+int Graph<NodeType, ArcType>::size() const {
+	return nodes.size();
+}
+
+template<typename NodeType, typename ArcType>
+bool Graph<NodeType, ArcType>::isEmpty() const {
+	return nodes.empty();
+}
+
+template<typename NodeType, typename ArcType>
+void Graph<NodeType, ArcType>::clear() {
+	for (NodeType* n : nodes) {
+		delete n;	//	nullptrは代入しない。set内のtreeを維持するため
+	}
+	for (ArcType* a : arcs) {
+		delete a;
+	}
+
+	nodes.clear();
+	arcs.clear();
+	nodeMap.clear();
+}
+
+template<typename NodeType, typename ArcType>
+void Graph<NodeType, ArcType>::addNode(const std::string& name) {
+	if (nodeMap.contains(name)) {
+		std::cerr << "ERROR : void Graph<NodeType, ArcType>::addNode(const std::string& name) : "
+			<< "すでにGraph内にnameを名前とするノードが存在します" << std::endl;
+		std::exit(EXIT_FAILURE);
+	}
+	NodeType* newNode = new NodeType;
+	newNode->name = name;
+	addNode(newNode);
+}
+
+template<typename NodeType, typename ArcType>
+void Graph<NodeType, ArcType>::addNode(NodeType* node) {
+	nodes.insert(node);
+	nodeMap.insert(std::pair(node->name, node));
+}
+
+template<typename NodeType, typename ArcType>
+void Graph<NodeType, ArcType>::removeNode(const std::string& name) {
+	removeNode(nodeMap[name]);
+}
+
+template<typename NodeType, typename ArcType>
+void Graph<NodeType, ArcType>::removeNode(NodeType* node) {
+	Vector<ArcType*> removeArcs;
+	for (ArcType* a : arcs) {
+		if (a->start == node || a->end == node) {
+			removeArcs += a;
+		}
+	}
+	for (int i = 0; i < removeArcs.size(); ++i) {
+		removeArc(removeArcs[i]);
+	}
+
+	nodes.erase(node);
+	delete node;
+	node = nullptr;
+}
+
+template<typename NodeType, typename ArcType>
+NodeType* Graph<NodeType, ArcType>::getNode(const std::string& name) const {
+	return nodeMap[name];
+}
+
+template<typename NodeType, typename ArcType>
+void Graph<NodeType, ArcType>::addArc(const std::string& s1, const std::string& s2) {
+	addArc(nodeMap[s1], nodeMap[s2]);
+}
+
+template<typename NodeType, typename ArcType>
+void Graph<NodeType, ArcType>::addArc(NodeType* n1, NodeType* n2) {
+	ArcType newArc = new ArcType;
+	newArc->start = n1;
+	newArc->end = n2;
+
+	addArc(newArc);
+}
+
+
+template<typename NodeType, typename ArcType>
+void Graph<NodeType, ArcType>::addArc(ArcType* arc) {
+	arcs->insert(arc);
+	arc->start->insert(arc);
+}
+
+template<typename NodeType, typename ArcType>
+void Graph<NodeType, ArcType>::removeArc(const std::string& s1, const std::string& s2) {
+	removeNode(nodeMap[s1], nodeMap[s2]);
+}
+
+template<typename NodeType, typename ArcType>
+void Graph<NodeType, ArcType>::removeArc(NodeType* n1, NodeType* n2) {
+	for (ArcType* a : n1->arcs) {
+		if (a->end == n2) {	//	a->startは必ずn1
+			arcs->erase(a);
+			n1->arcs.erase(a);
+			delete a;
+			a = nullptr;
+			return;
+		}
+	}
+}
+
+template<typename NodeType, typename ArcType>
+void Graph<NodeType, ArcType>::removeArc(ArcType* arc) {
+	NodeType* n1 = arc->start;
+	for (ArcType* a : n1->arcs) {
+		if (a == arc) {
+			arcs.erase(a);
+			n1->arcs.erase(a);
+			delete a;
+			a = nullptr;
+			return;
+		}
+	}
+}
+
+
+template<typename NodeType, typename ArcType>
+bool Graph<NodeType, ArcType>::isConnected(const std::string& s1, const std::string& s2) const {
+	return isConnected(nodeMap[s1], nodeMap[s2]);
+}
+
+
+template<typename NodeType, typename ArcType>
+bool Graph<NodeType, ArcType>::isConnected(NodeType* n1, NodeType* n2) const {
+	for (ArcType* a : n1->arcs) {
+		if (a->end == n2) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+
+template<typename NodeType, typename ArcType>
+std::set<NodeType*>& Graph<NodeType, ArcType>::getNodeSet() const {
+	return nodes;
+}
+
+
+template<typename NodeType, typename ArcType>
+std::set<ArcType*>& Graph<NodeType, ArcType>::getArcSet() const {
+	return arcs;
+}
+
+template<typename NodeType, typename ArcType>
+std::set<ArcType*>& Graph<NodeType, ArcType>::getArcSet(const std::string& name) const {
+	return nodeMap[name]->arcs;
+}
+
+
+template<typename NodeType, typename ArcType>
+std::set<ArcType*>& Graph<NodeType, ArcType>::getArcSet(NodeType* node) const {
+	return node->arcs;
+}
+
+template<typename NodeType, typename ArcType>
+std::set<NodeType*> Graph<NodeType, ArcType>::getNeighbors(const std::string& name) const {
+	return getNeighbors(nodeMap[name]);
+}
+
+template<typename NodeType, typename ArcType>
+std::set<NodeType*> Graph<NodeType, ArcType>::getNeighbors(NodeType* node) const {
+	std::set<NodeType*> s;
+	for (ArcType* a : node->arcs) {
+		s.insert(a->end);
+	}
+
+	return s;
+}
+
+template<typename NodeType, typename ArcType>
+Graph<NodeType, ArcType>& Graph<NodeType, ArcType>::operator=(const Graph<NodeType, ArcType>& src) {
+	if (this != &src) {	//	(*this!=src)はダメ。ポインタ変数を比較したほうが確実
+		clear();
+		deepCopy(src);
+	}
+
+	return *this;
+}
+
+template<typename NodeType, typename ArcType>
+void Graph<NodeType, ArcType>::deepCopy(const Graph<NodeType, ArcType>& src) {
+	for (NodeType* n : src.nodes) {
+		NodeType* nc = new NodeType;
+		*nc = *n;
+		nc->arcs.clear();
+		addNode(nc);
+	}
+
+	for (ArcType* a : src.arcs) {
+		ArcType* ac = new ArcType;
+		*ac = *a;
+		ac->start = nodeMap[a->start->name];
+		ac->end = nodeMap[a->end->name];
+		addArc(ac);
+	}
+}
